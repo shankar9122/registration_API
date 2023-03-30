4497411ced4d514cd146f477e97ae59ab64dc176
shankar9122 <web4shankar@gmail.com>
2023-03-30 18:56:25 +0530

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieparser());
 app.use(cors({
-    origin: ["http://localhost:3000/", "http://localhost:3001/", "http://localhost:3002/"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
